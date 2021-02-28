80b66e2eaf950d36a8927d9ecb83283bbdaab0e3
noflytm <61331881+noflytm@users.noreply.github.com>
2021-02-28 20:05:06 +0300

    task5_fix2

--- 5task.py
+++ 5task.py
@@ -7,23 +7,24 @@ Given a lower and upper number bound, output a list of every possible
 self dividing number, including the bounds if possible.
 """
 
-def CheckDivide(input):
-    def selfcheck(s):
-        for num in str(s):
-            if num == '0' or s % int(num) > 0:
+def checkdivide(inp):
+    def selfcheck(sin):
+        for num in str(sin):
+            if num == '0' or sin % int(num) > 0:
                 return False
         return True
 
     outputs = []
-    for s in range(input[0], input[1] + 1):
+    for s in range(inp[0], inp[1] + 1):
+
         if selfcheck(s):
             outputs.append(s)
     return outputs
 
 
 # Input values
-input = [int(x) for x in input().split(' ')]
+inputnum = [int(x) for x in input().split(' ')]
 
 
 # Print result of function
-print(CheckDivide(input))
+print(checkdivide(inputnum))
